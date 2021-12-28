50a8683da37a6e631b962f36a53864a2f314756c
Abi Raja <abimanyuraja@gmail.com>
2021-12-28 13:22:36 -0500

    Upgrade webpack and setup a webpack config that works with nextjs

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/core/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+    library: {
+      name: 'webpackNumbers',
+      type: 'umd',
+    },
+    publicPath: '',
+    globalObject: 'this'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          }
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+};
